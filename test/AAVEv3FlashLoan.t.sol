d0f769bc3758815c37d56117cc3930c3628f4dcd
MaxKuo <max2571112@gmail.com>
2023-12-11 16:15:57 +0800

    finished setup for flash loan

---
+++ test/AAVEv3FlashLoan.t.sol
@@ -0,0 +1,45 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.13;
+
+import {MyScript} from "../script/CompoundV2.s.sol";
+import {Test} from "../lib/forge-std/src/Test.sol";
+import {Script, console2} from "../lib/forge-std/src/Script.sol";
+import {CompoundV2SetUp} from "../test/helper/CompoundV2SetUp.sol";
+
+contract FlashLoanTest is MyScript, Test, CompoundV2SetUp {
+    uint256 mainnetFork;
+
+    function setUp() public override {
+        super.setUp();
+
+        mainnetFork = vm.createFork(
+            "https://mainnet.infura.io/v3/d5aad10125ce4463972de51361f5e5de"
+        );
+        vm.selectFork(mainnetFork);
+
+        deal(address(tokenA), user2, 1e22);
+        deal(address(tokenB), user2, 1e22);
+    }
+
+    function test_flash_loan_liquidate_with_AAVEv3() public {
+        vm.startPrank(user1);
+
+        tokenA.approve(address(cTokenA), type(uint256).max);
+        cTokenA.mint(1e21);
+        assertEq(cTokenA.balanceOf(user1), 1e21);
+
+        address[] memory cTokens = new address[](1);
+        cTokens[0] = address(cTokenA);
+        ComptrollerProxy_.enterMarkets(cTokens);
+        cTokenB.borrow(2.5e21);
+        assertEq(cTokenB.balanceOf(user1), 2.5e21);
+
+        vm.stopPrank();
+
+        vm.startPrank(admin);
+        SimplePriceOracle_.setUnderlyingPrice(CToken(addressc(TokenB)), 4e18);
+        vm.stopPrank();
+
+        vm.startPrank(user2);
+    }
+}
---
+++ test/helper/CompoundV2SetUp.sol
@@ -0,0 +1,83 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.13;
+
+import {MyScript} from "../../script/CompoundV2.s.sol";
+import {Test} from "../../lib/forge-std/src/Test.sol";
+import {Script, console2} from "../../lib/forge-std/src/Script.sol";
+import "../../lib/compound-protocol/contracts/Comptroller.sol";
+import "../../lib/compound-protocol/contracts/ComptrollerG7.sol";
+import "../../lib/compound-protocol/contracts/Unitroller.sol";
+import "../../lib/compound-protocol/contracts/CErc20Delegate.sol";
+import "../../lib/compound-protocol/contracts/CErc20Delegator.sol";
+import "../../lib/compound-protocol/contracts/WhitePaperInterestRateModel.sol";
+import "../../lib/compound-protocol/contracts/SimplePriceOracle.sol";
+import "../../lib/openzeppelin-contracts/contracts/token/ERC20/ERC20.sol";
+import {ERC20Token} from "../../src/ERC20Token.sol";
+
+contract CompoundV2SetUp is MyScript, Test {
+    address public admin = makeAddr("admin");
+    address public user1 = makeAddr("user1");
+    address public user2 = makeAddr("user2");
+
+    ERC20 public tokenA = ERC20(0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48);
+    ERC20 public tokenB = ERC20(0x1f9840a85d5aF5bf1D1762F925BDADdC4201F984);
+
+    function setUp() public virtual {
+        //oracle
+        SimplePriceOracle_ = new SimplePriceOracle();
+        //impl comptroller
+        Comptroller_ = new Comptroller();
+        //proxy comptroller
+        Unitroller_ = new Unitroller();
+
+        //proxy delegatecall to set address of comptroller
+        Unitroller_._setPendingImplementation(address(Comptroller_));
+        Comptroller_._become(Unitroller_);
+        ComptrollerProxy_ = ComptrollerG7(address(Unitroller_));
+        //Interest rate model
+        WhitePaperInterestRateModel_ = new WhitePaperInterestRateModel(0, 0);
+
+        //impl CErc20
+        CErc20Delegate_ = new CErc20Delegate();
+        //proxy CErc20
+        cTokenA = new CErc20Delegator(
+            address(throwokenA),
+            ComptrollerInterface(address(Unitroller_)),
+            WhitePaperInterestRateModel_,
+            1e18,
+            "cToken of Underlying TokenA",
+            "cUTKA",
+            18,
+            payable(admin),
+            address(CErc20Delegate_),
+            ""
+        );
+
+        cTokenB = new CErc20Delegator(
+            address(tokenB),
+            ComptrollerInterface(address(Unitroller_)),
+            WhitePaperInterestRateModel_,
+            1e18,
+            "cToken of Underlying TokenB",
+            "cUTKB",
+            18,
+            payable(admin),
+            address(CErc20Delegate_),
+            ""
+        );
+        //add cTokenA and cTokenB to market
+        ComptrollerProxy_._supportMarket(CToken(address(cTokenA)));
+        ComptrollerProxy_._supportMarket(CToken(address(cTokenB)));
+        //set price oracle
+        ComptrollerProxy_._setPriceOracle(SimplePriceOracle_);
+        //set cTokenA and cTokenB price
+        SimplePriceOracle_.setUnderlyingPrice(CToken(address(cTokenA)), 1e18);
+        SimplePriceOracle_.setUnderlyingPrice(CToken(address(cTokenB)), 5e18);
+        //set cTokenB collateral factor
+        ComptrollerProxy_._setCollateralFactor(CToken(address(cTokenB)), 5e17);
+        //set close factor
+        ComptrollerProxy_._setCloseFactor(5e17);
+        //set liquidation incentive
+        ComptrollerProxy_._setLiquidationIncentive(1.08 * 1e18);
+    }
+}
